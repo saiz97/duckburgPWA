d81e29fe0eef38b20f0fcff4b7e73da81066c636
Saiz <daniel.saiz@gmx.at>
2021-06-14 22:33:44 +0200

    added dataservice and interceptor

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private BASE_URL: string = "https://api.s1810456031.student.kwmhgb.at/wp-json/duckburg/";
+
+  isLoading = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) { }
+
+  getAllComics(params: { [index: string]: any } = {}): Observable<any> {
+    return this.http.get(`${this.BASE_URL}/comics`, { headers: new HttpHeaders(), params: this.handleParameters(params) })
+      .pipe(retry(3)).pipe(catchError(this.errorHandler));
+  }
+
+  getAllFigures(params: { [index: string]: any } = {}): Observable<any> {
+    return this.http.get(`${this.BASE_URL}/figures`, { headers: new HttpHeaders(), params: this.handleParameters(params) })
+      .pipe(retry(3)).pipe(catchError(this.errorHandler));
+  }
+
+  getAllClothes(params: { [index: string]: any } = {}): Observable<any> {
+    return this.http.get(`${this.BASE_URL}/clothes`, { headers: new HttpHeaders(), params: this.handleParameters(params) })
+      .pipe(retry(3)).pipe(catchError(this.errorHandler));
+  }
+
+  private handleParameters(params: { [index: string]: any } = {}) : HttpParams {
+    const parameters = new HttpParams();
+    Object.entries(params).forEach(([key, value]) => {
+      parameters.set(key, value);
+    });
+    return parameters;
+  }
+
+  private errorHandler(error: Error | any): Observable<any> {
+    return throwError(error);
+  }
+}
---
+++ src/app/service/token.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DataService } from './data.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private dataService: DataService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.dataService.isLoading.next(true);
+    request = request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+      }
+    })
+
+    return next.handle(request);
+  }
+}
